773388a1e79b7453e97b050a06ad9a8a11da08ee
hujio362 <hujio362@users.noreply.github.com>
2020-11-24 18:11:46 +0900

    ビューファイルの条件分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true,uniqueness: { case_sensitive: true }
   # validates :email,  inclusion: { in: ["@"] }
-  validates_format_of :encrypted_password, :with => /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/,length: { minimum: 6 }, :message => "は6文字以上の英数混在で入力してください。" 
+  validates_format_of :password, :with => /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/,length: { minimum: 6 }, :message => "は6文字以上の英数混在で入力してください。" 
   validates :birthday, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
