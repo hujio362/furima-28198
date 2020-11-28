c19937b4465e2438cd1036915bb8f9ee4e7218d8
hujio362 <hujio362@users.noreply.github.com>
2020-11-28 12:19:15 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
 
+  has_one :order
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+
+  validates :price, presence: true,
+            if: {  :greater_than_or_equal_to => 300,
+                :less_than_or_equal_to => 9999999,
+                :message => '入力値が¥300~¥9,999,999の範囲外です' }
+  
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_cost_id, presence: true
+  validates :area_id, presence: true
+  validates :day_id, presence: true
 end
