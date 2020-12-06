ac954a597ef69aa2f5f164e6696879281c274ef4
hujio362 <hujio362@users.noreply.github.com>
2020-12-06 11:35:06 +0900

    インデントを揃える

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', () => {
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  const profitDom = document.getElementById("profit");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
 
-  addTaxDom.innerHTML = 
-  `${tax = Math.floor(inputValue / 10)}`
+    addTaxDom.innerHTML = 
+    `${tax = Math.floor(inputValue / 10)}`
 
-  profitDom.innerHTML =
-  `${profit = (inputValue - tax)}`
-})
+    profitDom.innerHTML =
+    `${profit = (inputValue - tax)}`
+  })
 })
 
 
