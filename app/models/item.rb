7cb2adc3077b2114d885cb02515836a528e21a6d
hujio362 <hujio362@users.noreply.github.com>
2020-12-06 12:56:49 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,21 +3,27 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name               , presence: true
-  validates :explanation        , presence: true
-  validates :price              , presence: true , numericality: {  greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :image              , presence: true
-  validates :category_id        , numericality: { other_than: 1 }
-  validates :status_id          , numericality: { other_than: 1 }
-  validates :delivery_cost_id   , numericality: { other_than: 1 }
-  validates :area_id            , numericality: { other_than: 1 }
-  validates :day_id             , numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :image
+  end
+
+  validates :price  , presence: true , numericality: {  greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id 
+    validates :status_id
+    validates :delivery_cost_id
+    validates :area_id
+    validates :day_id
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :area
-  belongs_to_active_hash :category
-  belongs_to_active_hash :day
-  belongs_to_active_hash :delivery_cost
-  belongs_to_active_hash :status
+    belongs_to_active_hash :area
+    belongs_to_active_hash :category
+    belongs_to_active_hash :day
+    belongs_to_active_hash :delivery_cost
+    belongs_to_active_hash :status
     
   end
